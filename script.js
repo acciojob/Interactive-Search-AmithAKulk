8ce5ca9dd9697e3c9b570c8874a4eed7c65c4c4a
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-28 14:59:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const button = document.querySelector(".btn");
+button.addEventListener("click", ()=>{
+	const input = document.querySelector(".input");
+	input.focus();
+	const search = document.querySelector(".search");
+	search.classList.add("active")
+})
